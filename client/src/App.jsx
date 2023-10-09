9e965448c88c78aad56a16074e27f6c64bbcb772
wwCharles <charleswaweruw@gmail.com>
2023-10-09 11:28:06 +0300

    header and search ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+  // <div>IUIOUIOU</div>
   )
 }
