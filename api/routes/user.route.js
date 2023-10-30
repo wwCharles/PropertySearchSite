b47bd5f45bad4b6e3ec21604ff0ab46da2381eb3
wwCharles <charleswaweruw@gmail.com>
2023-10-30 13:11:08 +0300

    add firebase

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -13,6 +13,6 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
-// router.get("/:id", verifyToken, getUser);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
